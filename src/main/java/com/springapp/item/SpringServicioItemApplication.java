7bee3eb9c33879e2a24205cb7faaea22c2b96989
Oscar Padilla <osckarpgonzalez@gmail.com>
2022-10-16 16:57:41 -0400

    Primer commit servicio item

---
+++ src/main/java/com/springapp/item/SpringServicioItemApplication.java
@@ -0,0 +1,13 @@
+package com.springapp.item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringServicioItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringServicioItemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springapp/item/SpringServicioItemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springapp.item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringServicioItemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
